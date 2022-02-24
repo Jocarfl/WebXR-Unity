55834d354a263f1a9c8fd484154f2ba19654eb5b
Josep Carreres <jcarreresjr@hotmail.com>
2022-02-24 12:47:50 +0100

    Comentarios

--- Assets/WebXR-Pruebas/Scripts/WristTracking.cs
+++ Assets/WebXR-Pruebas/Scripts/WristTracking.cs
@@ -9,40 +9,32 @@ using Microsoft.MixedReality.Toolkit.Utilities;
 
 public class WristTracking : MonoBehaviour
 {
-    public GameObject sphereMarker;
-
-    GameObject sampleObject;
-
+    public GameObject prefabBoton;
+    private GameObject botonMunyeca;
 
     void Start()
     {
-        sampleObject = Instantiate(sphereMarker,this.transform);
-        
-        
+        // Instanciar el botón en la escena
+        botonMunyeca = Instantiate(prefabBoton,this.transform);
+               
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if(HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, Handedness.Right, out MixedRealityPose pose))
         {
+            //Renderizar el botón al detectar la muñeca de la mano derecha
+            botonMunyeca.GetComponent<Renderer>().enabled = true;
 
-            sampleObject.GetComponent<Renderer>().enabled = true;
-
+            //Ajustar la posición del botón   
+            botonMunyeca.transform.position = pose.Position + new Vector3(0.0f, 0.04f, 0.0f);
             
-            sampleObject.transform.position = pose.Position + new Vector3(0.0f, 0.04f, 0.0f);
-            
-            //Añadir 90 grados l eje X e Y para ajustar la rotación en la mano
+            //Añadir 90 grados al eje X e Y para ajustar la rotación en la mano
             var rotation = Quaternion.Euler(90,90,0);
-            sampleObject.transform.rotation = pose.Rotation * rotation;
-            
-            
-
-        }else
-        {
-            sampleObject.GetComponent<Renderer>().enabled = false;
-
+            botonMunyeca.transform.rotation = pose.Rotation * rotation;
         }
-
     }
+
+
 }
