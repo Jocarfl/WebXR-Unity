861063e45b418c2e9402132a9c75a1805d85310d
Josep Carreres <jcarreresjr@hotmail.com>
2022-02-28 13:12:49 +0100

    Rotación arreglada

--- Assets/WebXR-Pruebas/Scripts/WristTracking.cs
+++ Assets/WebXR-Pruebas/Scripts/WristTracking.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 
@@ -14,26 +15,13 @@ public class WristTracking : MonoBehaviour
 
     void Start()
     {
-        // Instanciar el botón en la escena
-        botonMunyeca = Instantiate(prefabBoton,this.transform);
-               
-    }
-
-    
-    void Update()
-    {
-        if(HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, Handedness.Right, out MixedRealityPose pose))
+        var handJointService = CoreServices.GetInputSystemDataProvider<IMixedRealityHandJointService>();
+        if (handJointService != null)
         {
-            //Renderizar el botón al detectar la muñeca de la mano derecha
-            botonMunyeca.GetComponent<Renderer>().enabled = true;
-
-            //Ajustar la posición del botón   
-            botonMunyeca.transform.position = pose.Position + new Vector3(0.0f, 0.04f, 0.0f);
-            
-            //Añadir 90 grados al eje X e Y para ajustar la rotación en la mano
-            var rotation = Quaternion.Euler(90,90,0);
-            botonMunyeca.transform.rotation = pose.Rotation * rotation;
+           Transform jointTransform = handJointService.RequestJointTransform(TrackedHandJoint.Wrist, Handedness.Right);
+            botonMunyeca = Instantiate(prefabBoton,jointTransform);  
         }
+   
     }
 
 
